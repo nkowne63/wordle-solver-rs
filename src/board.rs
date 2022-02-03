dc74d71feea1cdfd43157cad9e8b1e740ab298cf
Nkowne63 <neutron63zf@gmail.com>
2022-02-03 23:03:59 +0900

    add: print

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,5 @@
 use crate::enums::{Status, Word};
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Instant};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct WordPair(Word, Word);
@@ -29,6 +29,7 @@ impl Board {
         }
     }
     pub fn filter(&mut self, word: &Word, status: &Status) {
+        let start = Instant::now();
         let &mut Board {
             status_board: _,
             ref mut remaining_canditates,
@@ -36,14 +37,24 @@ impl Board {
             word_color_grouping: _,
             word_avg_info: _,
         } = self;
-        let remaining = remaining_canditates
+        let before_len = remaining_canditates.len();
+        let remaining: Vec<Word> = remaining_canditates
             .iter()
             .filter(|&answer| status == &Word::to_status(word, answer))
             .cloned()
             .collect();
+        let after_len = remaining.len();
         *remaining_canditates = remaining;
+        let end = start.elapsed();
+        println!("filter: {} -> {}", before_len, after_len);
+        println!(
+            "gained information: {}",
+            (before_len as f64 / after_len as f64).log2()
+        );
+        println!("filter time: {:?}", end);
     }
     pub fn compute_board(&mut self) {
+        let start = Instant::now();
         let &mut Board {
             ref mut remaining_canditates,
             ref mut input_canditates,
@@ -52,14 +63,31 @@ impl Board {
             word_avg_info: _,
         } = self;
         *status_board = HashMap::new();
-        input_canditates.iter().for_each(|word| {
-            remaining_canditates.iter().for_each(|answer| {
-                let status = Word::to_status(word, answer);
-                status_board.insert(WordPair(*word, *answer), status);
+        let input_len = input_canditates.len() as u32;
+        let mut current = 0f64;
+        input_canditates
+            .iter()
+            .enumerate()
+            .for_each(|(i_index, word)| {
+                remaining_canditates
+                    .iter()
+                    .enumerate()
+                    .for_each(|(_a_index, answer)| {
+                        let status = Word::to_status(word, answer);
+                        status_board.insert(WordPair(*word, *answer), status);
+                    });
+                let percentage = (i_index as f64 / input_len as f64) * 100.0;
+                let percentage = ((percentage / 5.0).floor() as i64 * 5) as f64;
+                if percentage >= (current + 5.0) {
+                    current = (current + 5.0).max(percentage);
+                    println!("board percentage {}%", current);
+                }
             });
-        });
+        let end = start.elapsed();
+        println!("board time: {:?}", end);
     }
     pub fn compute_info(&mut self) {
+        let start = Instant::now();
         let &mut Board {
             ref mut remaining_canditates,
             ref mut input_canditates,
@@ -68,32 +96,46 @@ impl Board {
             ref mut word_avg_info,
         } = self;
         let len_of_input_canditates = input_canditates.len() as u32;
-        input_canditates.iter().for_each(|word| {
-            remaining_canditates.iter().for_each(|answer| {
-                let status = status_board.get(&WordPair(*word, *answer)).unwrap();
-                word_color_grouping.insert(
-                    WordStatusPair(*word, *status),
-                    *word_color_grouping
-                        .get(&WordStatusPair(*word, *status))
-                        .unwrap_or(&0)
-                        + 1,
-                );
-            });
-            let mut avg_info = 0f64;
-            Status::get_status_iter().for_each(|status| {
-                let count = *word_color_grouping
-                    .get(&WordStatusPair(*word, status))
-                    .unwrap_or(&0);
-                if count == 0 {
-                    return;
+        let input_len = input_canditates.len() as u32;
+        let mut current = 0f64;
+        input_canditates
+            .iter()
+            .enumerate()
+            .for_each(|(i_index, word)| {
+                remaining_canditates.iter().for_each(|answer| {
+                    let status = status_board.get(&WordPair(*word, *answer)).unwrap();
+                    word_color_grouping.insert(
+                        WordStatusPair(*word, *status),
+                        *word_color_grouping
+                            .get(&WordStatusPair(*word, *status))
+                            .unwrap_or(&0)
+                            + 1,
+                    );
+                });
+                let mut avg_info = 0f64;
+                Status::get_status_iter().for_each(|status| {
+                    let count = *word_color_grouping
+                        .get(&WordStatusPair(*word, status))
+                        .unwrap_or(&0);
+                    if count == 0 {
+                        return;
+                    }
+                    let probablity = count as f64 / len_of_input_canditates as f64;
+                    avg_info += -probablity * probablity.log2();
+                });
+                word_avg_info.insert(*word, avg_info);
+                let percentage = (i_index as f64 / input_len as f64) * 100.0;
+                let percentage = ((percentage / 5.0).floor() as i64 * 5) as f64;
+                if percentage >= (current + 5.0) {
+                    current = (current + 5.0).max(percentage);
+                    println!("info percentage {}%", current);
                 }
-                let probablity = count as f64 / len_of_input_canditates as f64;
-                avg_info += -probablity * probablity.log2();
             });
-            word_avg_info.insert(*word, avg_info);
-        });
+        let end = start.elapsed();
+        println!("info time: {:?}", end);
     }
     pub fn compute_next_word_info(&self) -> (Word, f64) {
+        let start = Instant::now();
         let &Board {
             remaining_canditates: _,
             input_canditates: _,
@@ -105,6 +147,10 @@ impl Board {
             .iter()
             .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
             .unwrap();
+        let end = start.elapsed();
+        println!("search time: {:?}", end);
+        println!("next word: {}", word.to_string());
+        println!("next word info: {}", info);
         (*word, *info)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ pub mod commands;
 mod enums;
 mod words;
 
+use std::time::Instant;
+
 use board::Board;
 use commands::{ReplCommandHandlers, ReplFunctions};
 use enums::{Status, Word};
@@ -37,10 +39,24 @@ impl ReplFunctions for ReplFunctionsStruct {
     }
 
     fn next(board: &mut Board) -> Word {
+        let start = Instant::now();
         board.compute_board();
         board.compute_info();
+        let end = start.elapsed();
+        println!("compute next time: {:?}", end);
         board.compute_next_word_info().0
     }
 }
 
 impl ReplCommandHandlers for ReplFunctionsStruct {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    #[ignore]
+    fn get_maximum() {
+        let mut board = ReplFunctionsStruct::reset();
+        ReplFunctionsStruct::next(&mut board);
+    }
+}
