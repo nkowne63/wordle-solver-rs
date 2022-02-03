819bc2bcef96111e60cff9493615aa4a1b13d42e
Nkowne63 <neutron63zf@gmail.com>
2022-02-03 21:38:53 +0900

    add: board

--- src/board.rs
+++ src/board.rs
@@ -28,9 +28,39 @@ impl Board {
             word_avg_info,
         }
     }
-    fn prepare_state(&mut self) {
+    fn filter(&mut self, word: &Word, status: &Status) {
         let &mut Board {
-            // given
+            status_board: _,
+            ref mut remaining_canditates,
+            input_canditates: _,
+            word_color_grouping: _,
+            word_avg_info: _,
+        } = self;
+        let remaining = remaining_canditates
+            .iter()
+            .filter(|&answer| status == &Word::to_status(word, answer))
+            .cloned()
+            .collect();
+        *remaining_canditates = remaining;
+    }
+    fn compute_board(&mut self) {
+        let &mut Board {
+            ref mut remaining_canditates,
+            ref mut input_canditates,
+            ref mut status_board,
+            word_color_grouping: _,
+            word_avg_info: _,
+        } = self;
+        *status_board = HashMap::new();
+        input_canditates.iter().for_each(|word| {
+            remaining_canditates.iter().for_each(|answer| {
+                let status = Word::to_status(word, answer);
+                status_board.insert(WordPair(*word, *answer), status);
+            });
+        });
+    }
+    fn compute_info(&mut self) {
+        let &mut Board {
             ref mut remaining_canditates,
             ref mut input_canditates,
             ref mut status_board,
@@ -40,12 +70,11 @@ impl Board {
         let len_of_input_canditates = input_canditates.len() as u32;
         input_canditates.iter().for_each(|word| {
             remaining_canditates.iter().for_each(|answer| {
-                let status = Word::to_status(word, answer);
-                status_board.insert(WordPair(*word, *answer), status);
+                let status = status_board.get(&WordPair(*word, *answer)).unwrap();
                 word_color_grouping.insert(
-                    WordStatusPair(*word, status),
+                    WordStatusPair(*word, *status),
                     *word_color_grouping
-                        .get(&WordStatusPair(*word, status))
+                        .get(&WordStatusPair(*word, *status))
                         .unwrap_or(&0)
                         + 1,
                 );
@@ -55,10 +84,65 @@ impl Board {
                 let count = *word_color_grouping
                     .get(&WordStatusPair(*word, status))
                     .unwrap_or(&0);
+                if count == 0 {
+                    return;
+                }
                 let probablity = count as f64 / len_of_input_canditates as f64;
                 avg_info += -probablity * probablity.log2();
             });
             word_avg_info.insert(*word, avg_info);
         });
+    }
+    fn compute_next_word_info(&self) -> (Word, f64) {
+        let &Board {
+            remaining_canditates: _,
+            input_canditates: _,
+            status_board: _,
+            word_color_grouping: _,
+            ref word_avg_info,
+        } = self;
+        let (word, info) = word_avg_info
+            .iter()
+            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+            .unwrap();
+        (*word, *info)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn filter() {
+        let mut board = Board::new(
+            vec!["abcde", "fghij", "klmno", "pqrst"]
+                .iter()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+            vec!["afkpz", "fghij", "klmno", "pqrst"]
+                .iter()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
+        board.filter(&"afpkz".parse().unwrap(), &"_y___".parse().unwrap());
+        assert_eq!(board.remaining_canditates, vec!["fghij".parse().unwrap()]);
+    }
+    #[test]
+    fn info() {
+        let mut board = Board::new(
+            vec!["abcde", "fghij", "klmno", "pqrst"]
+                .iter()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+            vec!["afkpz", "fghij", "klmno", "pqrst"]
+                .iter()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
+        board.compute_board();
+        board.compute_info();
+        let (next, info) = board.compute_next_word_info();
+        assert_eq!(next, "afkpz".parse().unwrap());
+        assert_eq!(info, 2f64);
     }
 }
